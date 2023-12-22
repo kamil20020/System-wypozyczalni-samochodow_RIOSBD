aa2f9b5e1afe4272a2eaf76cd557955b9ef37505
kdywan_254049 <idywan_254049@e-science.pl>
2023-12-22 23:39:16 +0100

    Updatedd sql scripts

--- backend/src/main/resources/db/schema.sql
+++ backend/src/main/resources/db/schema.sql
@@ -3,34 +3,34 @@
 -- Create Cars table
 CREATE TABLE IF NOT EXISTS Cars (
     ID INT PRIMARY KEY AUTO_INCREMENT,
-    Manufacturer VARCHAR(50),
-    Model VARCHAR(50),
-    Type VARCHAR(50),
-    Color VARCHAR(20),
-    Licence_number VARCHAR(20),
-    Cost_15min DECIMAL(10, 2),
+    Manufacturer VARCHAR(50) NOT NULL,
+    Model VARCHAR(50) NOT NULL,
+    Type VARCHAR(50) NOT NULL,
+    Color VARCHAR(20) NOT NULL,
+    Licence_number VARCHAR(20) NOT NULL,
+    Cost_15min DECIMAL(10, 2) NOT NULL,
     Photo BLOB -- Assuming Photo is a binary field (BLOB)
 );
 
 -- Create Clients table
 CREATE TABLE IF NOT EXISTS Clients (
     ID INT PRIMARY KEY AUTO_INCREMENT,
-    Name VARCHAR(50),
-    Last_name VARCHAR(50),
-    Phone_number INT,
-    Email VARCHAR(100),
-    Birth_date DATE,
-    Client_code INT
+    Name VARCHAR(50) NOT NULL,
+    Last_name VARCHAR(50) NOT NULL,
+    Phone_number INT NOT NULL,
+    Email VARCHAR(100) NOT NULL,
+    Birth_date DATE NOT NULL,
+    Client_code INT NOT NULL
 );
 
 -- Create Cars_Clients table
 CREATE TABLE IF NOT EXISTS Cars_Clients (
     ID INT PRIMARY KEY AUTO_INCREMENT,
-    KlientID INT,
-    CarsID INT,
-    Rental_date DATE,
-    Return_date DATE,
-    Total_cost DECIMAL(10, 2),
+    KlientID INT NOT NULL,
+    CarsID INT NOT NULL,
+    Rental_date DATE NOT NULL,
+    Return_date DATE NOT NULL,
+    Total_cost DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY (KlientID) REFERENCES Clients(ID),
     FOREIGN KEY (CarsID) REFERENCES Cars(ID)
 );
